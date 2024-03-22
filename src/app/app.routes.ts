58b1d9b56b484cb69678d2078587410ee462bb58
Ezequiel Chain <ezequiel.chain@allata.com>
2024-03-22 17:28:47 -0300

    second page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Profiles2Component } from './pages/profiles2/profiles2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'profiles2',
+    component: Profiles2Component
+}];
---
+++ src/app/pages/profiles2/profiles2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profiles2',
+  standalone: true,
+  imports: [],
+  templateUrl: './profiles2.component.html',
+  styleUrl: './profiles2.component.scss'
+})
+export class Profiles2Component {
+
+}
