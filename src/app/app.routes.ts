0ca3e6a0c11bd1d5e01a5f37a05370c2e146d5e6
Ezequiel Chain <ezequiel.chain@allata.com>
2024-03-25 18:25:46 -0300

    page2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { Profiles2Component } from './pages/profiles2/profiles2.component';
 
 export const routes: Routes = [
     {
         component: ProfileComponent,
         path:'profile'
+    },
+    {
+        path:'profiles2',
+        component: Profiles2Component
     }
 ];
-import { Profiles2Component } from './pages/profiles2/profiles2.component';
 
-export const routes: Routes = [{
-    path:'profiles2',
-    component: Profiles2Component
-}];
